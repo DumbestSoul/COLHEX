c9802a13bdb0c5568da895d79ab4e044c168f3b6
Rajat Kishore Acharya <rajat.getmeonfb@gmail.com>
2023-03-19 21:40:10 +0530

    RGB to HEX conversion done. With string output

--- App.jsx
+++ App.jsx
@@ -1,6 +1,32 @@
 import './css/App.css';
+import { useState } from 'react';
+
+
+function componentToHex(c) {
+    let hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+    return componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
 
 export default function App(){
+    // react hooks
+    const [hexcode, setHexCode] = useState('ffffff')
+
+    
+    function handleClick(){
+        const rval = +document.querySelector('.r').value;
+        const gval = +document.querySelector('.g').value;
+        const bval = +document.querySelector('.b').value;
+        let res;
+        if((rval<0 || rval>255) || (gval<0 || gval>255) || (bval<0 || bval>255))
+            res = "INVALID";
+        else
+            res = rgbToHex(rval, gval, bval);
+        setHexCode(() => res);
+    }
+
     return (
         <div className="main">
             <div className="container">
@@ -11,13 +37,13 @@ export default function App(){
                 </div>
                 <hr />
                 <div className="output">
-                    HEX : <p className="hex-code">#234567</p>
+                    HEX : <p className="hex-code">#{hexcode}</p>
                     <div className="output-box">
 
                     </div>
                 </div>
                 <div className="submit-button">
-                    <button>Convert</button>
+                    <button onClick={handleClick}>Convert</button>
                 </div>
             </div>
         </div>
